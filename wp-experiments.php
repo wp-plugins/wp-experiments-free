846fd8ffb8bee85058a87ab530bae05dcbb08672
jasonlfunk <jasonlfunk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-17 22:19:20 +0000

    tagging version 2.4 which remove the upsell feature because it's not allowed in wordpress repo
    git-svn-id: https://plugins.svn.wordpress.org/wp-experiments-free/trunk@933989 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-experiments.php
+++ wp-experiments.php
@@ -5,7 +5,7 @@
 	Description: A/B test the titles of your pages and posts to get the most page views. More info: http://wpexperiments.com
 	Author: Jason Funk
 	Author URI: http://jasonfunk.net
-	Version: 2.3
+	Version: 2.4
 	License: GPLv3
 */
 
