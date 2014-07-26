2dd15e5118a903cc4abaf0f202774da334650d24
jasonlfunk <jasonlfunk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-26 16:28:29 +0000

    Update to version 3.4 for bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/wp-experiments-free/tags/3.4@955330 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpex-general-settings.php
@@ -0,0 +1,45 @@
+<div class='wrap'>
+	<div id="icon-edit-pages" class="icon32 icon32-posts-page"><br></div>
+	<h2>Title Experiments Settings</h2>
+	
+	<form method="post">
+		<table class="form-table">
+			<tbody>
+				<tr valign="top">
+					<th scope="row"><label for="use_js">Use JavaScript</label></th>
+					<td>
+						<input name="use_js" type="checkbox" value="1" <?php if($use_js): ?>checked<?php endif; ?>> Use JavaScript to set titles
+						<p class="description">Use JavaScript to set the titles. Use this if your site uses heavy caching.</p>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row"><label for="best_feed">Best title in feed</label></th>
+					<td>
+						<input name="best_feed" type="checkbox" value="1" <?php if($best_feed): ?>checked<?php endif; ?>> Use the best performing title in feeds
+						<p class="description">Use the best performing title in feeds instead of the default title.</p>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row"><label for="adjust_every">Recalculate every</label></th>
+					<td>
+						<select name="adjust_every">
+							<option value="0" <?php if($adjust_every == 0): ?>selected<?php endif; ?>>Instantly</option>
+							<option value="300" <?php if($adjust_every == 300): ?>selected<?php endif; ?>>5 minutes</option>
+							<option value="1800" <?php if($adjust_every == 1800): ?>selected<?php endif; ?>>30 minutes</option>
+							<option value="3600" <?php if($adjust_every == 3600): ?>selected<?php endif; ?>>1 hour</option>
+							<option value="7200" <?php if($adjust_every == 7200): ?>selected<?php endif; ?>>2 hours</option>
+							<option value="14400" <?php if($adjust_every == 14400): ?>selected<?php endif; ?>>4 hours</option>
+							<option value="28800" <?php if($adjust_every == 28800): ?>selected<?php endif; ?>>8 hours</option>
+						</select>
+						<p class="description">Recalculate title display probabilities every so often. Doing it too often can slow down high traffic sites.</p>
+					</td>
+				</tr>
+				<tr>
+					<th></th>
+					<td><input type="submit" class="button-primary" name="save" value="Save Settings" /></td>
+				</tr>
+			</tbody>
+		</table>
+	</form>
+	<p>For more information, visit <a href='https://wpexperiments.com/title-experiments/'>wpexperiments.com/title-experiments/</a></p>
+</div>
